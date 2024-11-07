3a93b32b213e17bbeb53f45d95cc23513decda57
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-07 18:33:54 +0530

    Added logic for deleting the note

--- node.js
+++ node.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.post('/save', async (req, res) => {
     try {
-        //Creating new node with data from the request body
+        //Creating new note with data from the request body
         const newNote = new Note({
             //id: ,
             title: req.body.title,
@@ -40,6 +40,31 @@ app.post('/save', async (req, res) => {
         });
     }
 })
+
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        const noteID = req.params.id;
+        const deletedNote = await Note.findByIdAndDelete(noteID);
+
+        if(!deletedNote) {
+            return res.status(404).json({
+                message: "Note not found",
+            })
+        }
+        
+        const remainingNotes = await Note.find();
+        res.status(200).json({
+            message: 'Note deleted successfully!',
+            deletedNote: deletedNote,
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Server Error',
+            error: error.message,
+        });
+    }
+});
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Server is running on ${port}`));
